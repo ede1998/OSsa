1f4cd17a3c802108ced64e357a6d2967c5d1d0c9
ede1998 <online@erik-hennig.me>
2019-08-12 21:32:34 +0200

    post04 finished: test harness

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(ossa::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
+use ossa::println;
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some Panic");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic_handler(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic_handler(info: &PanicInfo) -> ! {
+    ossa::test_panic_handler(info)
 }
